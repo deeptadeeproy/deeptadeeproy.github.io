d75b9585b109c779cd16ec903c3ce26c635d2859
droy <deeptadeeproy@yahoo.in>
2024-07-08 23:42:42 +0530

    fixed contact spelling

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -53,7 +53,7 @@ function Contact() {
           <h4>
             Please feel free to reach out to me via email, phone,
             <br/>
-            LinkedIn or the cantact form.
+            LinkedIn or the contact form.
             Let's build something awesome!
             <br />
             I will respond as soon as possible.
