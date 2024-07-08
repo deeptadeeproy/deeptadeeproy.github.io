164006ed3742b378ac8a7f9df9c639b06623458d
droy <deeptadeeproy@yahoo.in>
2024-07-09 00:16:50 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsDir: 'src/assets', // Ensure assets are copied to a directory named 'assets'
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Disable manual chunks
+      },
+    },
+  },
+});
