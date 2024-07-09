12f392eb238ab06074f2c82d0ca0a266cce29659
droy <deeptadeeproy@yahoo.in>
2024-07-09 11:15:05 +0530

    fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: 'assets', // Ensure assets are copied to a directory named 'assets'
+    assetsDir: "src/assets", // Ensure assets are copied to a directory named 'assets'
     rollupOptions: {
       output: {
         manualChunks: undefined, // Disable manual chunks
       },
     },
   },
+  base: "/",
 });
